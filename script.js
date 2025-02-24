03b8190025e5b41d1d4f501423fb05763550aa05
ahmedkhaled1994 <13239017+ahmedkhaled1994@users.noreply.github.com>
2025-02-24 22:28:02 +0200

    basic css and helper functions added

---
+++ script.js
@@ -0,0 +1,38 @@
+let operand1, operan2, operator;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+function updateDisplay(value) {
+    document.getElementById('display').value = value;
+}
+
+function buttonPress(value) {
+    updateDisplay(value);
+}
